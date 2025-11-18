652e9a288354d195871227d09ab5c9bf7f47fb9a
jatkinson1000 <jwa34@cam.ac.uk>
2025-11-18 12:53:02 +0000

    chore: clean trajectory_analysis files with fortitude
    * Strip trailing whitespaces
    * Update outdated syntax
    * fix missing syntax in declarations

--- trajectory_analysis/ts_tools.f90
+++ trajectory_analysis/ts_tools.f90
@@ -1,9 +1,9 @@
   module ts_tools_mod
   implicit none
   contains
- 
+
 !####################################################################
-  
+
   SUBROUTINE TS_FILTER (nlat, slat)
   implicit none
 
@@ -11,22 +11,22 @@
 !------------------------------------------------------------------------
 ! --- for land mask
 !------------------------------------------------------------------------
-  integer, parameter :: ix0 =   40                         
-  integer, parameter :: iy0 = 1620 
-  integer, parameter ::  ix = 360                         
-  integer, parameter ::  iy = 180 
-  real,    parameter ::  pi = 180.0  
+  integer, parameter :: ix0 =   40
+  integer, parameter :: iy0 = 1620
+  integer, parameter ::  ix = 360
+  integer, parameter ::  iy = 180
+  real,    parameter ::  pi = 180.0
   real,    parameter :: tpi = 360.0
   real,    parameter :: hpi =  90.0
 
-  character*120 :: &
+  character(len=120) :: &
   landmask = '/home/fms/local/opt/fre-analysis/test/keo/tropical_storms/trajectory_analysis/landsea.map'
 
   integer, dimension(ix0,iy0) :: mask0
   integer, dimension(ix, iy ) :: mask
   real                        :: dlon, lon0, dlat, lat0
 
-  integer, dimension(2) :: shape2 = (/ ix, iy /)
+  integer, dimension(2) :: shape2 = [ ix, iy ]
 
 !------------------------------------------------------------------------
 ! --- for latitude bounds
@@ -39,7 +39,7 @@
   real        :: xcyc, ycyc,  wind, psl, vmax
   integer     :: year, month, day,  hour, ii, jj, max
   logical     :: keeper
-  character*5 :: dummy
+  character(len=5) :: dummy
 
 !========================================================================
 
@@ -53,10 +53,10 @@
 ! --- set up land mask
 !------------------------------------------------------------------------
 
-   OPEN ( 10, FILE = trim(landmask) ) 
+   OPEN ( 10, FILE = trim(landmask) )
    do jj = 1,iy0
        read ( 10, 200 ) (mask0(ii,jj), ii=1,ix0)
-   enddo    
+   enddo
    CLOSE( 10 )
 
    mask = RESHAPE( mask0, shape2 )
@@ -64,8 +64,8 @@
 
    lon0 =  0.0
    dlon =  tpi / FLOAT( ix )
-   lat0 = -hpi + 0.5 *   pi / FLOAT( iy )                       
-   dlat =       -2.0 * lat0 / FLOAT( iy - 1 )  
+   lat0 = -hpi + 0.5 *   pi / FLOAT( iy )
+   dlat =       -2.0 * lat0 / FLOAT( iy - 1 )
 
 !------------------------------------------------------------------------
 ! --- process data
@@ -82,14 +82,14 @@
   100 READ( 12,103,end=101 ) xcyc, ycyc, year, month, day, hour
 
 !--- filter by latitude
-    keeper = ( ycyc <= nlat ) .and. ( ycyc >= slat ) 
+    keeper = ( ycyc <= nlat ) .and. ( ycyc >= slat )
 
 !--- filter out storms over land
          jj = ( ycyc - lat0 ) / dlat + 1.5
          ii = ( xcyc - lon0 ) / dlon + 1.5
     if ( ii == 0  ) ii = ix
     if ( ii >  ix ) ii = ii - ix
-    keeper = keeper .and. ( mask(ii,jj) == 0 ) 
+    keeper = keeper .and. ( mask(ii,jj) == 0 )
 
 !--- write a record
     if( keeper ) then
@@ -144,11 +144,11 @@
   logical, intent(in) :: do_filt
 
 !-------------------------------------------------------------------
-  integer, parameter :: ix  =  360                         
-  integer, parameter :: iy  =  180 
+  integer, parameter :: ix  =  360
+  integer, parameter :: iy  =  180
   integer, parameter :: ireg = 12
 
-  real,    parameter ::   pi = 180.0  
+  real,    parameter ::   pi = 180.0
   real,    parameter ::  tpi = 360.0
   real,    parameter ::  hpi =  90.0
 
@@ -156,10 +156,10 @@
 
   integer, dimension(ix,iy) :: imask
 
-  character*2, dimension(ireg) :: bx
+  character(len=2), dimension(ireg) :: bx
   data bx /' G','WA','EA','WP','EP','NI','SI','AU','SP','SA','NH','SH'/
 
-  character*120 :: &
+  character(len=120) :: &
   cmask = '/home/fms/local/opt/fre-analysis/test/keo/tropical_storms/trajectory_analysis/imask_2'
 
 !-------------------------------------------------------------------
@@ -171,8 +171,8 @@
 
   integer, dimension(13,ireg) :: icnt
 
-  character*2, dimension(13) :: cmo = &
-  (/ ' J',' F',' M',' A',' M',' J',' J',' A',' S',' O',' N',' D','Yr' /)
+  character(len=2), dimension(13), parameter :: cmo = &
+  [ ' J',' F',' M',' A',' M',' J',' J',' A',' S',' O',' N',' D','Yr' ]
 
 !===================================================================
 
@@ -189,8 +189,8 @@
 
    lon0 =  0.0
    dlon =  tpi / FLOAT( ix )
-   lat0 =  hpi - 0.5 *   pi / FLOAT( iy )                       
-   dlat =        2.0 * lat0 / FLOAT( iy - 1 )  
+   lat0 =  hpi - 0.5 *   pi / FLOAT( iy )
+   dlat =        2.0 * lat0 / FLOAT( iy - 1 )
 
 !------------------------------------------------------------
 ! --- loop through file & count storms
@@ -226,7 +226,7 @@
    icnt(13,1:ireg) = SUM( icnt(1:12,1:ireg), 1 )  ! year
 
 !-----------------------------------------------------------
-! --- output storm counts 
+! --- output storm counts
 !------------------------------------------------------------
 
   OPEN( 12, file='stats' )
